be8bf4acb7530e3fc46fef307eebf06852932d11
Kevin Wang <kev.y.wang@gmail.com>
2012-09-16 01:09:57 -0700

    Moved old frameworks around and added armv7+armv7s for iOS 6 support

---
+++ Frameworks/armv7+armv7s (iOS 6 compatible)/Kamcord.framework/Versions/A/Headers/Common/Core/KCVideoStateAndViewManager.h
@@ -0,0 +1,78 @@
+//
+//  KCVideoStateAndViewManager.h
+//  cocos2d-ios
+//
+//  Created by Kevin Wang on 7/13/12.
+//  Copyright (c) 2012 Kamcord Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "KCVideoProcessingAndShareManager.h"
+
+@class KCUI;
+
+@interface KCVideoStateAndViewManager : NSObject<KCVideoProcessDelegate>
+
+// Everything we need before we put the job on the share queues
+@property (nonatomic, retain) KCUI * preUploadManager;
+
+@property (nonatomic, assign) UIViewController * parentViewController;
+
+// Audio background
+@property (nonatomic, retain) KCAudio * audioBackground;
+
+// Should the UI wait for conversion to finish before
+// dismissing the share view?
+@property (assign, nonatomic) BOOL enableSynchronousConversionUI;
+
+// Show video controls when the replay is presented?
+@property (assign, nonatomic) BOOL showVideoControlsOnReplay;
+
+// The location of the Kamcord directory
+@property (nonatomic, retain) NSURL * kamcordDirectory;
+
+
+// Video properties
+@property (nonatomic, assign) CGSize        dimensions;
+@property (nonatomic, assign) NSUInteger    bitrate;
+@property (nonatomic, assign) float         targetFPS;
+
+- (id)init;
+
+- (void)showView;
+- (BOOL)cancelConversionForLatestVideo;
+
+// Video
+- (BOOL)beginVideoForce:(BOOL)force;
+- (BOOL)endVideo;
+
+- (BOOL)endVideoAndDiscardVideo;
+- (BOOL)endVideoAndAddSounds:(NSArray *)sounds;
+
+- (BOOL)startRecording;
+- (BOOL)stopRecording;
+- (BOOL)pause;
+- (BOOL)resume;
+- (BOOL)isRecording;
+
+- (void)markAbsoluteTime:(CFAbsoluteTime)absoluteTime;
+
+// Sound
+- (KCAudio *)playAudioWithName:(NSString *)name
+                     extension:(NSString *)extension
+                        volume:(float)volume
+                          loop:(BOOL)loop;
+- (void)stopAllSounds:(KC_SOUND_TYPE)soundType;
+
+#if KCUNITY
+- (void)writeAudioData:(float [])data
+                length:(size_t)nsamples
+           numChannels:(int)numChannels;
+#endif
+
+- (void)dealloc;
+
+
+@end
